a4a98048a330c44e2ca6a90c58113aa8bc6c9e39
Ankan <mr.ankanmaity@gmail.com>
2023-07-31 12:09:53 +0530

    Modified

--- PROJECTS/10_Password_Generator.py
+++ PROJECTS/10_Password_Generator.py
@@ -2,9 +2,11 @@
 import random
 
 keys = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '!', '@', '#', '$', '%', '^', '&', '*', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+
 symbols = ['!', '@', '#', '$', '%', '^', '&', '*']
 num = ['0','1','2','3','4','5', '6', '7', '8', '9']
-len = random.randint(8, 17)
+len = random.randint(8, 16)
+
 password = ""
 
 for i in range(0, len):
@@ -15,15 +17,12 @@ if password.isalnum():
     for char in password:
         if char in symbols:
             count += 1
-        
-
 
 if count == 0:
     password.replace(password[2], str(random.randint(0, 10)))
 else:
     pass
-
-    
-print(f"Password: {password} of length: {len}")
+print("-------- PASSWORD GENERATOR --------\n")
+print(f"Generated Password --> {password} of length: {len}.\n")
 
 
--- PROJECTS/13_Magic_8_Ball.py
+++ PROJECTS/13_Magic_8_Ball.py
@@ -1,12 +1,30 @@
 #  Magic 8 Ball
 
-import wolframalpha
+import random
+import time
 
-app_id = "LYJ39T-78KE2TJ9WX"
-the_client = wolframalpha.Client(app_id)
-question = input("Ask Magic 8 Ball:  ")
-print("\n")
-print("----------Magic 8 Ball is thinking!!!----------\n")
-response = the_client.query(question)
-answer = next(response.results).text
-print(f"Magic 8 Ball says: {answer}.")
+
+print("-----Magic 8 Ball-----\n")
+name = input("What is your name? ")
+print(f"Hi {name}\n")
+
+fortunes = ["It is certain", "It is decidedly so", "Without a doubt", "Yes, definitely",
+               "You may rely on it", "As I see it, yes", "Most Likely", "Outlook Good",
+               "Yes", "Signs point to yes", "Reply hazy, try again", "Ask again later",
+               "Better not tell you now", "Cannot predict now", "Concentrate and ask again",
+               "Don't count on it", "My reply is no", "My sources say no", "Outlook not so good", "Very Doubtful"]
+
+while(True):
+
+    a = input("Do you have any questions? ")
+    if a.lower() == 'yes':     
+        question = input("Ask any question: ")
+        sleep_time = time.sleep(random.randint(0, 10))
+        print("Let Magic 8 Ball think.\n")
+        answer = random.choice(fortunes)
+        print(answer)
+    else:
+        print("Thank you.")
+
+
+
